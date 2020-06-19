83ceacabde65e8b0fc62c75d8bb907650cc37889
Enrique Esquinas <esquinas.enrique@gmail.com>
2020-06-19 12:30:23 +0200

    Copy images to dist/.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = config => {
+  
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+
   return {
     // Process MD files, data files and HTML files by Nunjucks.
     markdownTemplateEngine: 'njk',
