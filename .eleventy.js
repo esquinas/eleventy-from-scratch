fb7eafe2cf6af46729f9052ede210f973610e2d7
Enrique Esquinas <esquinas.enrique@gmail.com>
2020-06-19 11:49:40 +0200

    Use Nunjucks to process .md, .html and data files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = config => {
   return {
+    // Process MD files, data files and HTML files by Nunjucks.
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine:     'njk',
+    htmlTemplateEngine:     'njk',
+
     dir: {
       input:  'src',
       output: 'dist'
